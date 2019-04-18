0ccacaa0f38c358c544455a8ca121886027dc79e
Rachel Rodriguez <rachelrodriguez014@gmail.com>
2019-04-17 21:53:10 -0700

    Rachel second

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,11 +1,20 @@
-var search = $("#searchInput").val();
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=xoc8kmonGBULYdCHuIy1jeEEVWPTjur5";
+$("#searchButton").on("click", function(){
 
-$.ajax({
-    url: queryUrl,
-    method: 'GET'
-}).then(function(response){
-    console.log(response);
-    $("#articles").html(response.docs);
-})
+    event.preventDefault();
+
+    var search = $("#searchInput").val();
+    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=xoc8kmonGBULYdCHuIy1jeEEVWPTjur5";
+
+    $.ajax({
+        url: queryUrl,
+        method: 'GET'
+    }).then(function(response){
+        console.log(response);
+        $("#articles").text(JSON.stringify(response.response.docs[0].web_url));
+    });
+
+    
+});
+
+
 
